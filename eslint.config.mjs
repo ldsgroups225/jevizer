61e04d0fdd0e589cdae450b53caa17a87281cd0f
Darius Kassi <kassidarius@gmail.com>
2025-03-27 21:03:29 +0000

    feat: add new UI components shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,13 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
+  ignores: [
+    'node_modules',
+    'dist',
+    'build',
+    'public',
+    '**/*.test.ts',
+    '**/*.test.tsx',
+    'src/components/ui/**/*.tsx',
+  ],
 })
