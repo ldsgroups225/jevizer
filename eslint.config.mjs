1d2c58f242df2b9db92c97caa79aad412d81b8fa
Darius Kassi <kassidarius@gmail.com>
2025-03-30 15:27:53 +0000

    feat: update ESLint configuration and add Vitest for testing support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import antfu from '@antfu/eslint-config'
+import vitest from '@vitest/eslint-plugin'
 
 export default antfu({
   react: true,
 
   rules: {
     'node/prefer-global/process': 'off',
+    'react-hooks-extra/no-unnecessary-use-prefix': 'off',
+    ...vitest.configs.recommended.rules,
+    'vitest/max-nested-describe': ['error', { max: 3 }],
   },
 
   ignores: [
